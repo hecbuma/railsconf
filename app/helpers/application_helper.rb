d0ce64846e62a89f31a472f3d7318f7d3f5ffb52
Hector Bustillos <hector.bustillos@crowdint.com>
2013-04-29 12:13:38 -0700

    add helper and fix wed on my schedule

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def talk_id(speaker, event)
+    string = speaker ? speaker : event
+    string = if string
+      string.downcase.tr(' ', '_').tr('&','n')
+    else
+      "no_id"
+    end
+    string
+  end
 end
