e9fb4e5e9473d837672019fc5e7e44933a6ad731
aditya-kapoor <aditya.kapoor@vinsol.com>
2013-05-02 11:59:05 +0530

    added before filter for logged in user, and improved login with omniauth method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,26 @@
 class SessionsController < ApplicationController
 
+  before_filter :avoid_logged_in_user_to_sign_in, :except => :destroy
+
   def new
-    if current_user
-      redirect_to schedule_sessions_path()
-    end
   end
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.where(:provider => auth["provider"], :uid => auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to dashboard_index_url, :notice => "Signed in!"
   end
 
   def destroy
-    session[:user_id] = nil
+    session.reset
     redirect_to root_url, :notice => "Signed out!"
   end
+
+  private
+    def avoid_logged_in_user_to_sign_in
+      if current_user
+        redirect_to schedule_sessions_path, alert: "You have already logged in"
+      end
+    end
 end
